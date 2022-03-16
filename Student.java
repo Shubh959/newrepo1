9e6ea5aadb8b7fa065090e4165ef9ff21adc8520
Shubh959 <101618252+Shubh959@users.noreply.github.com>
2022-03-16 10:24:29 +0530

    student work done

---
+++ Student.java
@@ -0,0 +1,17 @@
+package Math2;
+
+public class Student {
+	/*int a;
+	float b;
+	char c;
+	boolean d;
+	String e;
+	
+	public static void main(String[] args) {
+	System.out.println());*/
+	string result1=mark
+	
+
+	}
+
+}
